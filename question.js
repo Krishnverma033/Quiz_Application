e2cf9e39e7e92e34acfd11588fa3132b81df379f
Krishnverma033 <167005275+Krishnverma033@users.noreply.github.com>
2024-04-14 20:07:51 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,133 @@
+let questions =[
+    {
+        numb : 1,
+        question : "What does HTML stand for?",
+        answer : "C. Hyper Text Markup Language",
+        option : [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    
+    },
+
+    {
+        numb : 2,
+        question : "Machine learning is a subset of which of the following.?",
+        answer : "A. Artificial intelligence",
+        option : [
+                    "A. Artificial intelligence",
+                    "B.Deep learning",
+                    "C. Data Learning",
+                    "D. None of the above"
+        ]
+    
+    },
+
+    {
+        numb : 3,
+        question : "Who is the father of machine learning.?",
+        answer : "A. Geoffrey Everest  Hinton",
+        option : [
+            "A. Geoffrey Everest  Hinton",
+            "B.Geoffrey Hill",
+            "C. Geoffrey Chaucer",
+            "D. None of the above"
+    
+        ]
+    
+    },
+
+    {
+        numb : 4,
+        question : "What is the extension type of the excel 2007 files.?",
+        answer : "B. .xlsx",
+        option : [
+                    "A. .xls",
+                    "B. .xlsx",
+                    "C. .xsl",
+                    "D. None of the above"
+    
+        ]
+    
+    },
+
+    {
+        numb : 5,
+        question : "In computer world, Trojan refer to. ?",
+        answer : "B. Malware",
+        option : [
+                    "A. Virus",
+                    "B. Malware",
+                    "C. Worm",
+                    "D. Spyware"
+        ]
+    
+    },
+
+    {
+        numb : 6,
+        question : "What is a variable in C programming?",
+        answer : "C. A storage location with a name",
+        option : [
+                    "A. A type of function",
+                    "B. An operator",
+                    "C. A storage location with a name",
+                    "D. A keyword in C"
+        ]
+    
+    },
+
+    {
+        numb : 7,
+        question : "Which of the following is a valid variable name in C?",
+        answer : "B. number1",
+        option : [
+                    "A. 1number",
+                    "B. number1",
+                    "C. number-1",
+                    "D. _number"
+        ]
+    
+    },
+
+    {
+        numb : 8,
+        question : "Which SQL statement is used to retrieve data from a database?",
+        answer : "D. SELECT",
+        option : [
+                    "A. INSERT",
+                    "B. DELETE",
+                    "C. UPDATE",
+                    "D. SELECT"
+        ]
+    
+    },
+
+    {
+        numb : 9,
+        question : "What is the purpose of normalization in a database?",
+        answer : "A. Reduce data redundancy",
+        option : [
+                    "A. Reduce data redundancy",
+                    "B. Increase complexity",
+                    "C. Increase data redundancy",
+                    "D. Improve data security"
+        ]
+    
+    },
+
+    {
+        numb : 10,
+        question : "Which data type is used in SQL to store true or false values?",
+        answer : "C. BOOLEAN",
+        option : [
+                    "A. VARCHAR",
+                    "B. FLOAT",
+                    "C. BOOLEAN",
+                    "D. INT"
+        ]
+    
+    }
+]
